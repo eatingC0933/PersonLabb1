080fb885d48d10d649987e33c81a67e7fe3d5c5b
Yiting Chang <yiting.chang@iths.se>
2025-05-05 15:05:55 +0200

    Update PersonRepository.java

--- src/main/java/se/yitingchang/personlabb1/PersonRepository.java
+++ src/main/java/se/yitingchang/personlabb1/PersonRepository.java
@@ -1,4 +1,19 @@
 package se.yitingchang.personlabb1;
 
-public interface PersonRepository {
-}
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+    List<Person> findByLastName(String lastName);
+    Optional<Person> findById(int id);
+    List<Person> findAll();
+    List<Person> findByAgeGreaterThan(int age);
+    List<Person> findByAgeLessThan(int age);
+    List<Person> findByEmail(String email);
+    List<Person> findByPhoneNumber(String phone);
+
+
+
+  }
